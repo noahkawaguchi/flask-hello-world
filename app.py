a573e2732eb8e96e0b71b84327b49c2627d3da41
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-03-20 14:37:48 +0000

    Add db_select route

--- app.py
+++ app.py
@@ -45,3 +45,22 @@ def inserting():
     conn.commit()
     conn.close()
     return 'Basketball Table Successfully Populated'
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://lab10pg_user:ELctjWkuSDdbYUS060ld3L6nFhOnpOBx@dpg-cve20odsvqrc73f708j0-a/lab10pg")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string = ''
+    response_string += '<table>'
+    for player in records:
+        response_string += '<tr>'
+        for info in player:
+            response_string += '<td>{}</td>'.format(info)
+        response_string += '</tr>'
+    response_string += '</table>'
+    return response_string
