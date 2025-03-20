f87c5032b7c98542a2873ebd840bfe1cd508a9ab
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-03-20 14:14:02 +0000

    Add db_test route

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Noah Kawaguchi in 3308'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://lab10pg_user:ELctjWkuSDdbYUS060ld3L6nFhOnpOBx@dpg-cve20odsvqrc73f708j0-a/lab10pg")
+    conn.close()
+    return 'Database Connection Successful'
+
