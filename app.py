0fac5fbe1dda21e712d53d77b1b6230e902d3728
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-03-20 15:05:25 +0000

    Add comments

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
+#
+# Purpose: To connect to a Render PostgreSQL instance from a Flask app 
+#          running on Render and set up various endpoints for the 
+#          following actions in accordance with the provided steps: 
+#           - Test app (hello world)
+#           - Test database
+#           - Create table
+#           - Insert data
+#           - Retrieve data and display it as an HTML table
+#           - Drop table
+# Author:  Noah Kawaguchi
+# Usage:   Navigate to the following routes: 
+#           - /
+#           - /db_test 
+#           - /db_create
+#           - /db_insert
+#           - /db_select
+#           - /db_drop
+#
+
 from flask import Flask
 import psycopg2
 
+
 app = Flask(__name__)
 
+
+# Display a simple message to confirm that the Flask app deployment is 
+# working and that the app has successfully been modified from the 
+# original template
 @app.route('/')
 def hello_world():
     return 'Hello World from Noah Kawaguchi in 3308'
 
+
+# Test the database connection
 @app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgresql://lab10pg_user:ELctjWkuSDdbYUS060ld3L6nFhOnpOBx@dpg-cve20odsvqrc73f708j0-a/lab10pg")
     conn.close()
     return 'Database Connection Successful'
 
+
+# Create the Basketball table
 @app.route('/db_create')
 def creating():
     conn = psycopg2.connect("postgresql://lab10pg_user:ELctjWkuSDdbYUS060ld3L6nFhOnpOBx@dpg-cve20odsvqrc73f708j0-a/lab10pg")
@@ -30,6 +59,8 @@ def creating():
     conn.close()
     return 'Basketball Table Successfully Created'
 
+
+# Insert the provided information into the Basketball table
 @app.route('/db_insert')
 def inserting():
     conn = psycopg2.connect("postgresql://lab10pg_user:ELctjWkuSDdbYUS060ld3L6nFhOnpOBx@dpg-cve20odsvqrc73f708j0-a/lab10pg")
@@ -46,6 +77,9 @@ def inserting():
     conn.close()
     return 'Basketball Table Successfully Populated'
 
+
+# Query the Basketball table and return a formatted table of information 
+# (HTML string)
 @app.route('/db_select')
 def selecting():
     conn = psycopg2.connect("postgresql://lab10pg_user:ELctjWkuSDdbYUS060ld3L6nFhOnpOBx@dpg-cve20odsvqrc73f708j0-a/lab10pg")
@@ -65,6 +99,8 @@ def selecting():
     response_string += '</table>'
     return response_string
 
+
+# Drop the Basketball table
 @app.route('/db_drop')
 def dropping():
     conn = psycopg2.connect("postgresql://lab10pg_user:ELctjWkuSDdbYUS060ld3L6nFhOnpOBx@dpg-cve20odsvqrc73f708j0-a/lab10pg")
